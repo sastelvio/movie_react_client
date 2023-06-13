daabfe89ad65c70d2ec139b89335501b315593cb
sastelvios <sast.serafim@gmail.com>
2023-06-14 01:03:52 +0200

    set css to carousel

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -10,9 +10,9 @@ const Hero = ({movies}) => {
                 {
                     movies?.map((movie) => {
                         return (
-                            <Paper>
+                            <Paper key={movie.imdbId}>
                                 <div className='movie-card-container'>
-                                    <div className='movie-card'>
+                                    <div className='movie-card' style={{"--img": `url(${movie.backdrops[0]})`}}>
                                         <div className='movie-detail'>
                                             <div className='movie-poster'>
                                                 <img src={movie.poster} alt='' />
